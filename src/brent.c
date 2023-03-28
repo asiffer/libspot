63f3bbffb9a703ba099b23fe67c5e8186e00b06c
Alban Siffer <alban.siffer@irisa.fr>
2023-03-28 22:03:14 +0200

    use numerical recipes brent method

--- src/brent.c
+++ src/brent.c
@@ -9,7 +9,9 @@
  */
 #include "brent.h"
 
-double const BRENT_DEFAULT_EPSILON = 0.0000001;
+double const BRENT_DEFAULT_EPSILON = 2.0e-8;
+
+unsigned long const BRENT_ITMAX = 200;
 
 static double fabs(double a) {
     if (a < 0) {
@@ -25,8 +27,8 @@ static void swap(double *x, double *y) {
 }
 
 // https://handwiki.org/wiki/Brent%27s_method
-double brent(int *found, double a, double b, real_function f, void *extra,
-             double epsilon) {
+double brent2(int *found, double a, double b, real_function f, void *extra,
+              double epsilon) {
     double fa = f(a, extra);
     double fb = f(b, extra);
     if (fa * fb >= 0) {
@@ -89,4 +91,93 @@ double brent(int *found, double a, double b, real_function f, void *extra,
 
     *found = 1;
     return b;
+}
+
+double brent(int *found, double x1, double x2, real_function func, void *extra,
+             double tol) {
+    // we assume we found the root
+    // it will be set to zero in error cases
+    *found = 1;
+
+    double a = x1;
+    double b = x2;
+    double c = x2;
+    double d = 0.0;
+    double e = 0.0;
+    double min1 = 0.0;
+    double min2 = 0.0;
+
+    double fa = func(a, extra);
+    double fb = func(b, extra);
+    double fc = 0.0;
+    double p = 0.0;
+    double q = 0.0;
+    double r = 0.0;
+    double s = 0.0;
+    double tol1 = 0.0;
+    double xm = 0.0;
+
+    if ((fa > 0.0 && fb > 0.0) || (fa < 0.0 && fb < 0.0)) {
+        *found = 0;
+        return 0.0;
+    }
+
+    fc = fb;
+    for (unsigned long iter = 0; iter < BRENT_ITMAX; iter++) {
+        if ((fb > 0.0 && fc > 0.0) || (fb < 0.0 && fc < 0.0)) {
+            c = a; // Rename a, b, c and adjust bounding interval
+            fc = fa;
+            e = d = b - a;
+        }
+        if (fabs(fc) < fabs(fb)) {
+            a = b;
+            b = c;
+            c = a;
+            fa = fb;
+            fb = fc;
+            fc = fa;
+        }
+        tol1 = 2.0 * BRENT_DEFAULT_EPSILON * fabs(b) +
+               0.5 * tol; // Convergence check.
+        xm = 0.5 * (c - b);
+        if (fabs(xm) <= tol1 || fb == 0.0)
+            return b;
+        if (fabs(e) >= tol1 && fabs(fa) > fabs(fb)) {
+            s = fb / fa; // Attempt inverse quadratic interpolation.
+            if (a == c) {
+                p = 2.0 * xm * s;
+                q = 1.0 - s;
+            } else {
+                q = fa / fc;
+                r = fb / fc;
+                p = s * (2.0 * xm * q * (q - r) - (b - a) * (r - 1.0));
+                q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+            }
+            if (p > 0.0)
+                q = -q; // Check whether in bounds.
+            p = fabs(p);
+            min1 = 3.0 * xm * q - fabs(tol1 * q);
+            min2 = fabs(e * q);
+            if (2.0 * p < (min1 < min2 ? min1 : min2)) {
+                e = d; // Accept interpolation.
+                d = p / q;
+            } else {
+                d = xm; // Interpolation failed, use bisection.
+                e = d;
+            }
+        } else { // Bounds decreasing too slowly, use bisection.
+            d = xm;
+            e = d;
+        }
+        a = b; // Move last best guess to a.
+        fa = fb;
+        if (fabs(d) > tol1) // Evaluate new trial root.
+            b += d;
+        else
+            b += ((xm) >= 0.0 ? fabs(tol1) : -fabs(tol1));
+        fb = func(b, extra);
+    }
+    // Maximum number of iterations exceeded
+    *found = 0;
+    return 0.0;
 }
